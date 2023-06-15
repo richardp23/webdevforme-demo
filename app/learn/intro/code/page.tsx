9604f55214a9a34c7719c28a714d959257acd111
Richard Perez Jr <richard.p23@icloud.com>
2023-06-15 15:19:33 -0400

    Replaced apostrophes with proper escapes

--- app/learn/intro/code/page.tsx
+++ app/learn/intro/code/page.tsx
@@ -7,10 +7,10 @@ export default function Page() {
                 <div className={"hero-body"}>
                     <div className={"container"}>
                         <p className={"title is-spaced has-text-info"}>
-                            Based on your answers: you're a beginner to web development.
+                            Based on your answers: you&apos;re a beginner to web development.
                         </p>
                         <p className={"subtitle has-text-danger-light"}>
-                            And that's okay, everyone starts off somewhere. Let's start with the basics!
+                            And that&apos;s okay, everyone starts off somewhere. Let&apos;s start with the basics!
                         </p>
                         <Link href={"./code/lesson1"} className={"button is-info"}>Continue</Link>
                     </div>
